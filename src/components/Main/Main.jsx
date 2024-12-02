244d2ae2e9ee3a6339255e6dfe35c6a5136e0883
Chathuranga Shakthi <chathurangashakthi.k@gmail.com>
2024-12-03 01:39:12 +0530

    main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Main.css'
+import { assets } from '../../assets/assets'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='main'>
+        <div className='nav'>
+            <p>Gemini</p>
+            <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello, dev.</span></p>
+                <p>How can i help you today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
