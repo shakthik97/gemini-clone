d894c3e6cf63f22c37a64aa2cacda8cfb800ef2d
Chathuranga Shakthi <chathurangashakthi.k@gmail.com>
2024-12-03 23:32:44 +0530

    API call

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main'
 
+
 const App = () => {
   return (
     <>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -19,6 +19,31 @@ const Main = () => {
                     <p>Suggest beautiful places to see on an upcoming road trip</p>
                     <img src={assets.compass_icon} alt="" />
                 </div>
+                <div className="card">
+                    <p>briefly summerize this concept: Urben planning</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Brainstorm team bonding activities for our work retreat</p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Improve the readability of the following code</p>
+                    <img src={assets.code_icon} alt="" />
+                </div>
+            </div>
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='Enter a prompt here' />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className="bottom-info">
+                    Gemini may display inacurate info, including about people, so double check its responses. Your privacy and Gemini apps.
+                </p>
             </div>
         </div>
     </div>
