0939f8c399f02a8e0ccaee4a1069d4161be6badf
Chathuranga Shakthi <chathurangashakthi.k@gmail.com>
2024-12-04 11:07:53 +0530

    new resopnse logic

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -12,6 +12,10 @@ const ContextProvider = (props) => {
     const [loading, setLoading] = useState(false);
     const [resultData, setResultData] = useState("");
 
+    const delayPara = (index, nextWord) => {
+
+    }
+
     const onSent = async (prompt) => {
 
         setResultData("")
@@ -19,7 +23,18 @@ const ContextProvider = (props) => {
         setShowResult(true)
         setRecentPrompt(input)
         const response = await run(input)
-        setResultData(response)
+        let responseArray = response.split("**");
+        let newResponse;
+        for(let i=0 ; i < responseArray.length; i++){
+            if(i === 0 || i%2 !== 1){
+                newResponse += responseArray[i];
+            }
+            else{
+                newResponse += "<b>" + responseArray[i] + "</b>";
+            }
+        }
+        let newResponse2 = newResponse.split("*").join("</br>")
+        setResultData(newResponse2)
         setLoading(false)
         setInput("")
      }
